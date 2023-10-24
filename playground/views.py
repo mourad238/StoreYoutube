ca01bf016ad4f9cc54f15385bf7c32c24cadef67
Darkyy69 <radiasweetgirl812@gmail.com>
2023-10-24 20:51:30 +0100

    Last Change 24-oct-23

--- playground/views.py
+++ playground/views.py
@@ -27,24 +27,10 @@ def create_todo_page(request):
 
     #Connect to the database
     all_tasks = Todo.objects.all() 
-    # html = '<ul id="task-list">'
-    # for task in all_tasks:
-    #     if task.isDone:
-    #         DoneTxt = 'Task Completed.'
-    #     else:
-    #         DoneTxt = 'Task Not Completed.'
-    #     html+= '<li>Todo-List_'+ str(task.id) + '  ||  ' + task.task_Text + '  ||  ' + str(task.date_created) + '  ||  ' + DoneTxt + '</li><br>'
-
-    # html += '</ul>'
-    # return HttpResponse(html)
     return render(request, 'create_Todo-List.html',{'all_tasks': all_tasks})
 
 
 def view_todo_page(request,task_id):
     #Connect to the database
     task_id_text = Todo.objects.get(id=task_id).task_Text
-    # html = ''
-    # for task in all_todo_lists:
-    #     url = '/index/todo_page/'+str(task.id)+'/'
-    #     html+= '<a href="'+ url +'">Todo-List_'+ str(task.id) +'</a><br>'
     return render(request, 'view_Todo-List.html', {'task_id': str(task_id), 'task_Text': task_id_text})
